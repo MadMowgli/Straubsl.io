805a0d36dba75c72dbbbfed284ba416dce83ca6c
MadMowgli <laeubin.j@gmail.com>
2023-01-05 20:38:29 +0100

    Updated results page

--- src/main/resources/static/result.js
+++ src/main/resources/static/result.js
@@ -1,10 +1,17 @@
-// Style table rows since Thymeleaf won't load the CSS properly
-const rows = document.getElementsByTagName('tr');
-for(let i = 0; i < rows.length; i++) {
-    rows[i].style.height = "5rem";
-}
+$(document).ready(function () {
 
-const data = document.getElementsByTagName('td');
-for(let i = 0; i < data.length; i++) {
-    data[i].style.height = "5rem";
-}
+    // Style table rows since Thymeleaf won't load the CSS properly
+    const rows = document.getElementsByTagName('tr');
+    for(let i = 0; i < rows.length; i++) {
+        rows[i].style.height = "5rem";
+    }
+
+    const data = document.getElementsByTagName('td');
+    for(let i = 0; i < data.length; i++) {
+        data[i].style.height = "5rem";
+    }
+
+
+})
+
+
